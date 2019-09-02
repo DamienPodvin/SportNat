301d64575a142b7c1ab964b7f329dee982e135d4
Votre nom ou pseudo <Votre@email.com>
2019-09-02 11:52:33 +0200

    Base du projet (master)

---
+++ SportNateProjet/src/main/java/fr/formation/inti/projet/SportNateProjetApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.inti.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportNateProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportNateProjetApplication.class, args);
+	}
+
+}
