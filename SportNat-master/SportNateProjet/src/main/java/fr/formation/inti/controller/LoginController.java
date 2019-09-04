0a0cfcfa7ea479ddd16bbac0e2de37a00ad1ea76
Votre nom ou pseudo <Votre@email.com>
2019-09-04 14:15:37 +0200

    all ok : login, agenda, accueil

--- SportNat-master/SportNateProjet/src/main/java/fr/formation/inti/controller/LoginController.java
+++ SportNat-master/SportNateProjet/src/main/java/fr/formation/inti/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 	
-@RequestMapping("/login")
+@RequestMapping("/")
 	public String login(Model model) {
 		 return "view/login";
 }
